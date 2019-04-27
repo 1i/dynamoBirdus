24a4722d504689f934e15ee5da16b9e728c7881d
Patrick O'Sullivan <patrick.osullivan.ie@gmail.com>
2019-04-27 20:45:56 +0100

    parser working draft

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Retrieve bird sightings from a website
-Parse the records and save into a DynamoDB
-Read the daily results from the DynamoDB
-List them out as an Alexa Skill
+## DynamoBirdus
+Retrieve bird sightings from a website  
+Parse the records and save into a DynamoDB  
+Read the daily results from the DynamoDB  
+List them out as an Alexa Skill  
+
+
+#### Parsing the html
+Find the correct results table  
+When using regexes can become confusing later on without notes  
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
@@ -13,71 +14,36 @@ public class Parser {
     public static void main(String args[]) throws IOException, URISyntaxException {
 
         URL resource = Parser.class.getClassLoader().getResource("result.html");
-
-
         File file = new File(resource.toURI());
         Document document = Jsoup.parse(file, "UTF-8");
+
         Elements tableText = document.getElementsByTag("tr");
 
-        String header = "<tr> \n" +
-                " <td><strong>No.</strong></td> \n" +
-                " <td><strong>Ref.</strong></td> \n" +
-                " <td><strong>Date</strong></td> \n" +
-                " <td><strong>Common Name</strong></td> \n" +
-                " <td><strong>Scientific Name</strong></td> \n" +
-                " <th><strong>Number</strong></th> \n" +
-                " <td><strong>Location</strong></td> \n" +
-                " <td><strong>County</strong></td> \n" +
-                " <th><strong>Photos</strong></th> \n" +
-                "</tr>";
-
+        //todo hardcoded location
         Elements results = tableText.get(4).parent().children();
-        System.out.println(tableText.size());
 
-        String[] split = results.toString().trim().split("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ");
-
-        //todo
-        ArrayList formatted = new ArrayList();
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].matches("(IB)\\d+") ||split[i].length()>1) {
-                formatted.add(split[i]);
-            }
-        }
         ArrayList models = new ArrayList();
-        for (int i = 0; i < formatted.size()-8; i +=8) {
-            String num = formatted.get(i).toString();
-            String id = formatted.get(i + 1).toString();
-            String date = formatted.get(i + 2).toString();
-            String commonName = formatted.get(i + 3).toString();
-            String sciName = formatted.get(i + 4).toString();
-            String count = formatted.get(i + 5).toString();
-            String location = formatted.get(i + 6).toString();
-            String county = formatted.get(i + 7).toString();
+        for (Element result : results) {
+            if (result.childNodes().size() == 19) {
 
-            Model Model = new Model(num, id, date, commonName, sciName, count, location, county);
-            System.out.println(Model);
-            models.add(Model);
+                //regex to remove between html tags
+                String num = result.childNodes().get(1).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String id = result.childNodes().get(3).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String date = result.childNodes().get(5).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String commonName = result.childNodes().get(7).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String sciName = result.childNodes().get(9).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String count = result.childNodes().get(11).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String location = result.childNodes().get(13).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+                String county = result.childNodes().get(15).toString().replaceAll("<(.|\\n)+?>|\n|^\\s*\\r?\\n| &nbsp; ", "");
+
+
+                Model Model = new Model(num, id, date, commonName, sciName, count, location, county);
+                System.out.println(Model);
+                models.add(Model);
+            }
+
         }
-
-        System.out.println("Model size "+ models.size());
-
-        Model result = (Model) models.get(1);
-        System.out.println(result);
-
-
-//        int start = tableText.indexOf("No. Ref. Date Common Name Scientific Name Number Location County Photos");
-//        String substring = tableText.substring(start);
-//
-//        Pattern compile = Pattern.compile("IB\\d\\d\\d\\d\\d\\d");
-//        Matcher matcher = compile.matcher(substring);
-//        matcher.matches();
-//        matcher.groupCount();
-//        String[] birds = substring.split("IB");
-//        System.out.println(birds.length);
-//        String[] byNumber = birds[1].split("\\s\\d\\s");
-//        System.out.println(substring);
-//        List<Node> nodes = document.childNodes();
-
+        System.out.println(models.size());
     }
 
 }
